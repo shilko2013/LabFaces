06627fb5437ea41e293a23c4a10caa92f898915e
shilko2013 <shilko2013@mail.ru>
2018-11-05 16:11:37 +0300

    Version 0.1. Create project structure.

---
+++ db.sql
@@ -0,0 +1,7 @@
+CREATE TABLE results (
+    id serial primary key,
+    x double precision not null,
+    y double precision not null,
+    r double precision not null,
+    checking boolean not null
+);
